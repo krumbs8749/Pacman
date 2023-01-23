43603ab4b6a62f76b2f106b9540ae75b19ccc4c2
Ikram Hafidz <69641252+krumbs8749@users.noreply.github.com>
2023-01-23 20:36:24 +0100

    Moved scripts to index.html

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const map = [
     ['4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3']
 ]
 const canvas = document.querySelector('canvas')
-console.log({canvas})
 const c = canvas.getContext('2d')
 const scoreEL = document.querySelector("#score")
 canvas.width = map[0].length*42
